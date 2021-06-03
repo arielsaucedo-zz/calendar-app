2aded6b58feb115f8c1c918cc28ceda451d95445
Oscar Ariel Saucedo <arielsaucedo@gmail.com>
2021-06-03 11:06:36 -0300

    Login y register de app conectada al backend

--- src/types/types.js
+++ src/types/types.js
@@ -7,4 +7,12 @@ export const types = {
   eventClearActiveEvent: "[Event] Clear Active Event",
   eventUpdated: "[Event] Event Updated",
   eventDeleted: "[Event] Event Deleted",
+
+  authChecking: "[Auth] Checking login state",
+  authCheckingFinish: "[Auth] Finish checking login state",
+  authStartLogin: "[Auth] Start login",
+  authLogin: "[Auth] Login",
+  authStartRegister: "[Auth] Start register",
+  authStartTokenRenew: "[Auth] Start token renew",
+  authLogout: "[Auth] Logout",
 };
